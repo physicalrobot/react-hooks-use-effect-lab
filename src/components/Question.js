953a500872a214506628a21ffdb2c72431a9f8b3
Vikalp Malhotra <kimboho@gmail.com>
2022-01-22 16:39:49 -0500

    commit tree 0dbd583ff746a23594eea3458feb1e05014c5ca9

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+
+
+  function changeTime() {
+    const newtime = timeRemaining - 1;
+    setTimeRemaining(newtime);
+  }
+
+  useEffect(() => {
+
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+
+    const timerId = setTimeout(changeTime, 1000);
+
+    return (() => {
+      clearTimeout(timerId)
+    });
+  }, [timeRemaining, onAnswered])
+
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
